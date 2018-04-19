4d7d53eef0b45656d4bc9e6d4c96dfdfeeeca7b5
christopher demeke <christopher.demeke@gmail.com>
2018-04-18 21:24:25 -0500

    update

---
+++ 05-Serve-Favorites/server.js
@@ -0,0 +1,40 @@
+// Dependencies
+var http = require("http");
+var fs = require("fs");
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+
+    var path = req.url;
+
+    // Depending on the URL, display a different HTML file.
+    switch (path) {
+
+        case "/food":
+            return fs.readFile(__dirname + "/index.html", function (err, data) {
+
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+        // case "/Movie":
+        //     return fs.readFile(__dirname + "/index.html", function (err, data) {
+
+        //         res.writeHead(200, { "Content-Type": "text/html" });
+        //         res.end(data);
+        //     });
+        default:
+            return fs.readFile(__dirname + "/home.html", function (err, data) {
+
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+    }
+}
+// Starts our server
+server.listen(PORT, function () {
+    console.log("Server is listening on PORT: " + PORT);
+});
+
