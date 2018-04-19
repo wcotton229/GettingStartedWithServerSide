a5aa0ac5c662b1d6709bd251f4ff8cdd7476ca7c
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-04-18 20:18:50 -0500

    Added Random comments

--- 02-Two-Servers/server1.js
+++ 02-Two-Servers/server1.js
@@ -1,11 +1,17 @@
 var http = require('http');
 var PORT1 = 7000;
 var PORT2 = 7500;
+
+
 function handleRequest1(request, response) {
-    response.end('You are a great developer!');
+    var comments1 = ['You are cool!', 'Good Job!', 'Keep it up!'];
+    var randomComps = comments1[Math.floor(Math.random() * 3)];
+    response.end(randomComps);
 }
 function handleRequest2(request, response) {
-    response.end('You need to stop getting distracted');
+    var comments2 = ['Oh, do not do that!', 'You are doing a poor job!', 'Get a life!'];
+    var randomInsults = comments2[Math.floor(Math.random() * 3)];
+    response.end(randomInsults);
 }
 
 var server1 = http.createServer(handleRequest1);
@@ -20,3 +26,8 @@ server2.listen(PORT2, function() {
     console.log('Listening on PORT: '+ PORT2);
     
     });
+
+
+
+
+
