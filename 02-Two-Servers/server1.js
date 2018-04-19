6d396e079faa8589b6ca4808a0c2d6d6153defb1
christopher demeke <christopher.demeke@gmail.com>
2018-04-18 19:29:39 -0500

    Updated servers two in one

--- 02-Two-Servers/server1.js
+++ 02-Two-Servers/server1.js
@@ -1,16 +1,22 @@
 var http = require('http');
-var PORT = 7000;
-
-function handleRequest(request, response) {
+var PORT1 = 7000;
+var PORT2 = 7500;
+function handleRequest1(request, response) {
     response.end('You are a great developer!');
 }
+function handleRequest2(request, response) {
+    response.end('You need to stop getting distracted');
+}
 
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
 
-server.listen(PORT, function() {
-console.log('Listening on PORT: '+ PORT);
-
-
-
+server1.listen(PORT1, function() {
+console.log('Listening on PORT: '+ PORT1);
 
 });
+
+server2.listen(PORT2, function() {
+    console.log('Listening on PORT: '+ PORT2);
+    
+    });
