0ca34297f5664ec29b8a499370ad34326b795517
christopher demeke <christopher.demeke@gmail.com>
2018-04-18 19:25:06 -0500

    Updated servers

--- 02-Two-Servers/server1.js
+++ 02-Two-Servers/server1.js
@@ -2,11 +2,15 @@ var http = require('http');
 var PORT = 7000;
 
 function handleRequest(request, response) {
-    response.end('It works Path:' + request.url);
+    response.end('You are a great developer!');
 }
 
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function() {
 console.log('Listening on PORT: '+ PORT);
+
+
+
+
 });
--- 02-Two-Servers/server2.js
+++ 02-Two-Servers/server2.js
@@ -2,11 +2,14 @@ var http = require('http');
 var PORT = 7500;
 
 function handleRequest(request, response) {
-    response.end('It works Path:' + request.url);
+    response.end('You need to stop getting distracted');
 }
 
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function() {
 console.log('Listening on PORT: '+ PORT);
+
+
+
 });
